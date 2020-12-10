a5a7f53d472c713c8d606fa8276e38e8a760df85
beforelight <1761690868@qq.com>
2020-12-10 23:53:26 +0800

    ???

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,25 @@
+#include "cmdline.h"
 #include <iostream>
 #include "prj.h"
-//#include "clipp.h"
 using namespace std;
-//using namespace clipp;
 int main(int argc, char *argv[]) {
-//    bool rec = false, utf16 = false;
-//    string infile = "", fmt = "csv";
-//    auto cli = (
-//            value("input file", infile),
-//                    option("-r", "--recursive").set(rec).doc("convert files recursively"),
-//                    option("-o") & value("output format", fmt),
-//                    option("-utf16").set(utf16).doc("use UTF-16 encoding")
-//    );
-
-
-
-//    if (!parse(argc, argv, cli)) {
-//        cout << make_man_page(cli, argv[0]);
-//    }
-    if(argc>=2){
-        prj_ptr ptr;
-        ptr.Load(argv[1]);
-        ptr->Find();
+    std::vector<std::string> args;
+    args.reserve(argc);
+    for (int i = 0; i < argc; ++i) {
+        args.emplace_back(argv[i]);
     }
-    std::cout << "Hello, World!" << std::endl;
+    cmdline::parser ps;
+    ps.add<string>("file", 'f', "input file about iar\\keil\\eclipse project",
+                   true, "");
+    ps.add<string>("type", 't', "output project files type",
+                   false, "all", cmdline::oneof<string>("vs", "vscode", "cmake", "all"));
+
+    if (args.size() == 2) {
+        args[1] = "--file=" + args[1];
+    }
+    ps.parse_check(args);
+    prj_ptr ptr;
+    ptr.Load(ps.get<string>("file"));
+    ptr->Find();
     return 0;
 }
